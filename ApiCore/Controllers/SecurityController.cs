7ffb333d772b4dce07b1b24197f5fd5d7f80cd71
Danny Cárdenas <danny.cardenas@bayteq.com>
2026-04-10 15:10:24 -0500

    Remove RSA and AES encryption endpoints from SecurityController to streamline the API and improve maintainability.

--- ApiCore/Controllers/SecurityController.cs
+++ ApiCore/Controllers/SecurityController.cs
@@ -92,34 +92,4 @@ public class SecurityController(IMediator mediator) : ApiControllerBase(mediator
     public async Task<IActionResult> GenerateBiometricChallenge([FromBody] GenerateBiometricChallengeRequest request)
         => Success(await Mediator.Send(request).ConfigureAwait(false));
 
-    /// <summary>
-    /// Encripta texto con AES
-    /// </summary>
-    /// <param name="request">Texto a encriptar</param>
-    /// <returns>Texto encriptado</returns>
-    [HttpPost("rsa/encrypt")]
-    [AllowAnonymous]
-    [ProducesResponseType(typeof(GenericResponse<RsaEncryptTextResponse>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> RsaEncrypt([FromBody] RsaEncryptTextRequest request)
-        => Success(await Mediator.Send(request).ConfigureAwait(false));
-
-    /// <summary>
-    /// Encripta texto con AES
-    /// </summary>
-    /// <param name="request">Filtros de consulta</param>
-    /// <returns>Texto encriptado</returns>
-    [HttpPost("aes/encrypt")]
-    [ProducesResponseType(typeof(GenericResponse<AesEncryptTextResponse>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> AesEncryptText([FromBody] AesEncryptTextRequest request)
-        => Success(await Mediator.Send(request).ConfigureAwait(false));
-
-    /// <summary>
-    /// Encripta texto con AES
-    /// </summary>
-    /// <param name="request">Filtros de consulta</param>
-    /// <returns>Texto encriptado</returns>
-    [HttpPost("aes/decrypt")]
-    [ProducesResponseType(typeof(GenericResponse<AesDecryptTextResponse>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> AesDecryptText([FromBody] AesDecryptTextRequest request)
-        => Success(await Mediator.Send(request).ConfigureAwait(false));
 }
